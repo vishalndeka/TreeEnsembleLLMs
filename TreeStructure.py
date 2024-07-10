e9d176f67d100ca9bf6cdfdf8c8c7c6146c35ff3
vishalndeka <72727433+vishalndeka@users.noreply.github.com>
2024-07-10 11:19:08 +0530

    added method that initializes heap and generate() (unused)

--- TreeStructure.py
+++ TreeStructure.py
@@ -1,5 +1,7 @@
 import heapq
 import random
+import requests
+import json
 
 # @dataclass(order=True)
 class LLMNode():
@@ -12,17 +14,42 @@ class LLMNode():
     
     def __repr__(self) -> str:
         return f"{self.model_name} with score {self.score}"
+    
+def generate(llm: str, prompt:str) -> str:
+    url = 'http://127.0.0.1:11434/api/generate'
+    f = open('input.json', 'r')
+    data = json.load(f)
+    data['model'] = llm
+    data['prompt'] = prompt
+    response = requests.post(url, json=data)
+    assert response.status_code == 200
+    dictionary = json.loads(response.text)
+    f.close()
+    return dictionary['response']
+
+def init_tree(llmList:list) -> list:
+    li = []
+    for _ in llmList:
+        li.append(LLMNode(_, random.random()))
+    heapq.heapify(li)
+    return li
 
 if __name__ == "__main__":
-    llama = LLMNode("llama", random.random())
-    gemma = LLMNode("gemma", random.random())
-    mistral = LLMNode("mistral", random.random())
-    deepseek = LLMNode("deepseek", random.random())
-    phi = LLMNode("phi", random.random())
+    llmNodes = init_tree(['llama', 'gemma', 'mistral', 'deepseek', 'phi'])
+    for _ in llmNodes:
+        print(_)
 
-    llmList = [llama, gemma, mistral, deepseek, phi]
-    heapq.heapify(llmList)
-    for llm in llmList:
-        print(llm)
+    ## querying, and got some updated scores say
+
+    score_phi = 1
+    for _ in llmNodes:
+        if _.model_name == 'phi':
+            _.score = score_phi
+            break
+    
+    heapq.heapify(llmNodes)
+    print('After updating score: ')
+    for _ in llmNodes:
+        print(_)
 
     
